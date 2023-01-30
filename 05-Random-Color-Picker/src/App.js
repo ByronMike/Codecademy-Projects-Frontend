f138e086a874718bb801b2ebc39962894b763452
Michael <auger.michaell@gmail.com>
2023-01-30 14:44:28 +0100

    commit

---
+++ 05-Random-Color-Picker/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+import Random from '../src/Random';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Random />
+      </div>
+    );
+  }
+}
+
+export default App;
